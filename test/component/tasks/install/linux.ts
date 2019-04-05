e40cb5c75d7a546ca118fd11d42f5757a1472c22
calebcartwright <caleb.cartwright@outlook.com>
2019-04-04 21:13:29 -0500

    tests(component): added install tests for linux

---
+++ test/component/tasks/install/linux.ts
@@ -0,0 +1,146 @@
+'use strict';
+
+import chai = require('chai');
+import Sinon = require('sinon');
+
+import ShellCheckVersion = require('../../../../src/tasks/install/shellcheck-version');
+import task = require('../../../../src/tasks/install/task');
+import utils = require('../utils');
+
+const assert = chai.assert;
+
+suite('Install on Linux', () => {
+    let taskLibDebugStub: Sinon.SinonStub;
+    let taskLibSetResultStub: Sinon.SinonStub;
+    let taskLibGetVersionInputStub: Sinon.SinonStub;
+    let osTypeStub: Sinon.SinonStub;
+    const failedResult = utils.failedResult;
+
+    setup(() => {
+        taskLibDebugStub = utils.getTaskLibDebugStub();
+        taskLibGetVersionInputStub = utils.getTaskLibGetVersionInputStub();
+        taskLibGetVersionInputStub.callsFake(() => ShellCheckVersion.stable);
+        taskLibSetResultStub = utils.getTaskLibSetResultStub();
+        osTypeStub = utils.getOsTypeStub();
+    });
+
+    teardown(() => {
+        Sinon.restore();
+    });
+
+    test('Should fail with correct error on invalid version input', async () => {
+        const invalidVersion = 'foo';
+        const expectedErrorMessage = utils.getInvalidVersionErrorMessage(invalidVersion);
+        taskLibGetVersionInputStub.callsFake(() => invalidVersion);
+        await task.run();
+        assert.isTrue(taskLibSetResultStub.calledWithExactly(failedResult, expectedErrorMessage, true));
+    });
+
+    test('Should fail with correct error when installer throws error without details', async () => {
+        osTypeStub.throws(() => new Error());
+        await task.run();
+        assert.isTrue(taskLibDebugStub.calledWithExactly('Error details: unknown'));
+        assert.isTrue(taskLibSetResultStub.calledWithExactly(failedResult, utils.taskFatalErrorMessage));
+    });
+
+    test('Should fail with correct error when installer throws error with details', async () => {
+        const details = 'oh nose';
+        osTypeStub.throws(() => new Error(details));
+        await task.run();
+        assert.isTrue(taskLibDebugStub.calledWithExactly(`Error details: ${details}`));
+        assert.isTrue(taskLibSetResultStub.calledWithExactly(failedResult, utils.taskFatalErrorMessage));
+    });
+
+    suite('version installation', () => {
+        let pathJoinStub: Sinon.SinonStub;
+        let taskLibGetVariableStub: Sinon.SinonStub;
+        let taskLibMkdirPStub: Sinon.SinonStub;
+        let osArchStub: Sinon.SinonStub;
+        let toolLibPrependPathStub: Sinon.SinonStub;
+        let toolLibDownloadStub: Sinon.SinonStub;
+        let taskLibToolStub: Sinon.SinonStub;
+        let toolRunnerArgStub: Sinon.SinonStub;
+        let toolRunnerExecStub: Sinon.SinonStub;
+        let binaryDirectoryName = '';
+        const downloadDirectory = '/foo/bar';
+        const tempDirectoryPath = '/vsts/work/_temp';
+        let extractRoot = '';
+        let binTargetDirectory = '';
+        let expectedTarArgs = [];
+        const shellCheckBinaryUrlBase = utils.shellCheckBinaryUrlBase;
+
+        setup(() => {
+            osTypeStub.callsFake(() => 'Linux');
+            osArchStub = utils.getOsArchStub();
+            pathJoinStub = utils.getPathJoinStub();
+            taskLibGetVariableStub = utils.getTaskLibGetVariableStub();
+            taskLibGetVariableStub.withArgs('Agent.TempDirectory').callsFake(() => tempDirectoryPath);
+            taskLibMkdirPStub = utils.getTaskLibMkdirPStub();
+            toolLibPrependPathStub = utils.getToolLibPrependPathStub();
+            toolLibDownloadStub = utils.getToolLibDownloadStub();
+            taskLibToolStub = utils.getTaskLibToolStub();
+            toolRunnerArgStub = utils.getToolRunnerArgStub();
+            toolRunnerExecStub = utils.getToolRunnerExecStub();
+        });
+
+        const setDynamicVariablesWithVersion = (version: string) => {
+            binaryDirectoryName = `shellcheck-${version}`;
+            extractRoot = `${tempDirectoryPath}/${binaryDirectoryName}`;
+            binTargetDirectory = `${extractRoot}/${binaryDirectoryName}`;
+            expectedTarArgs = [ 'xC', extractRoot, '-f', downloadDirectory ];
+            toolLibDownloadStub.callsFake(() => Promise.resolve(downloadDirectory));
+            pathJoinStub.withArgs(tempDirectoryPath, binaryDirectoryName).callsFake(() => extractRoot);
+            pathJoinStub.withArgs(extractRoot, binaryDirectoryName).callsFake(() => binTargetDirectory);
+        };
+
+        const validate = (expectedDownloadUrl: string) => {
+            assert.isTrue(toolLibDownloadStub.calledWithExactly(expectedDownloadUrl));
+            assert.isTrue(taskLibGetVariableStub.calledWithExactly('Agent.TempDirectory'));
+            assert.isTrue(taskLibMkdirPStub.calledOnceWithExactly(extractRoot));
+            assert.isTrue(taskLibToolStub.calledOnceWithExactly('tar'));
+            assert.isTrue(toolRunnerArgStub.calledOnceWithExactly(expectedTarArgs));
+            assert.isTrue(toolRunnerExecStub.calledOnce);
+            assert.isTrue(toolLibPrependPathStub.calledWithExactly(binTargetDirectory));
+        };
+
+        test('Should correctly install latest version on 64 bit architecture', async () => {
+            const version = ShellCheckVersion.latest;
+            taskLibGetVersionInputStub.callsFake(() => version);
+            setDynamicVariablesWithVersion(version);
+            osArchStub.callsFake(() => 'x64');
+            const expectedDownloadUrl = `${shellCheckBinaryUrlBase}/shellcheck-${version}.linux.x86_64.tar.xz`;
+            await task.run();
+            validate(expectedDownloadUrl);
+        });
+
+        test('Should correctly install latest version on 64 bit ARM architecture', async () => {
+            const version = ShellCheckVersion.latest;
+            taskLibGetVersionInputStub.callsFake(() => version);
+            setDynamicVariablesWithVersion(version);
+            osArchStub.callsFake(() => 'arm64');
+            const expectedDownloadUrl = `${shellCheckBinaryUrlBase}/shellcheck-${version}.linux.armv6hf.tar.xz`;
+            await task.run();
+            validate(expectedDownloadUrl);
+        });
+
+        test('Should correctly install stable version on 64 bit architecture', async () => {
+            const version = ShellCheckVersion.stable;
+            taskLibGetVersionInputStub.callsFake(() => version);
+            setDynamicVariablesWithVersion(version);
+            osArchStub.callsFake(() => 'x64');
+            const expectedDownloadUrl = `${shellCheckBinaryUrlBase}/shellcheck-${version}.linux.x86_64.tar.xz`;
+            await task.run();
+            validate(expectedDownloadUrl);
+        });
+
+        test('Should correctly install stable version on 64 bit ARM architecture', async () => {
+            const version = ShellCheckVersion.stable;
+            taskLibGetVersionInputStub.callsFake(() => version);
+            setDynamicVariablesWithVersion(version);
+            osArchStub.callsFake(() => 'arm64');
+            const expectedDownloadUrl = `${shellCheckBinaryUrlBase}/shellcheck-${version}.linux.armv6hf.tar.xz`;
+            await task.run();
+            validate(expectedDownloadUrl);
+        });
+    });
+});
--- test/component/tasks/utils.ts
+++ test/component/tasks/utils.ts
@@ -1,8 +1,11 @@
 'use strict';
 
 import os = require('os');
+import path = require('path');
 import Sinon = require('sinon');
 import taskLib = require('azure-pipelines-task-lib');
+import toolLib = require('azure-pipelines-tool-lib');
+import toolRunner = require('azure-pipelines-task-lib/toolrunner');
 
 const getTaskLibGetInputStub = () => Sinon.stub(taskLib, 'getInput');
 const getTaskLibSetResultStub = () => Sinon.stub(taskLib, 'setResult');
@@ -13,19 +16,52 @@ const getTaskLibGetVersionInputStub = (getInputStub?: Sinon.SinonStub) => {
     }
     return getInputStub.withArgs('version').callsFake(() => '');
 };
+
+const getTaskLibGetVariableStub = () => Sinon.stub(taskLib, 'getVariable');
+const getTaskLibMkdirPStub = () => Sinon.stub(taskLib, 'mkdirP');
+
 const getOsTypeStub = () => Sinon.stub(os, 'type').callsFake(() => '');
+const getOsArchStub = () => Sinon.stub(os, 'arch').callsFake(() => '');
+const getPathJoinStub = () => Sinon.stub(path, 'join');
+
+const toolRunnerStub: toolRunner.ToolRunner = <toolRunner.ToolRunner> {
+    arg: (_val) => null,
+    argIf: (_condition, _val) => null,
+    exec: (_options) => null
+};
+
+const getTaskLibToolStub = () => Sinon.stub(taskLib, 'tool').callsFake(() => toolRunnerStub);
+const getToolRunnerArgStub = () => Sinon.stub(toolRunnerStub, 'arg').callsFake(() => toolRunnerStub);
+const getToolRunnerArgIfStub = () => Sinon.stub(toolRunnerStub, 'argIf').callsFake(() => toolRunnerStub);
+const getToolRunnerExecStub = () => Sinon.stub(toolRunnerStub, 'exec').callsFake(() => toolRunnerStub);
+const getToolLibPrependPathStub = () => Sinon.stub(toolLib, 'prependPath');
+const getToolLibDownloadStub = () => Sinon.stub(toolLib, 'downloadTool');
+
 const getInvalidVersionErrorMessage = (version: string) => `Invalid Version: '${version}'. Allowed values are: latest, stable, 0.6.0, 0.5.0, 0.4.7, 0.4.6 .`;
 
 const failedResult = taskLib.TaskResult.Failed;
 const taskFatalErrorMessage = 'Fatal error. Enable debugging to see error details.';
+const shellCheckBinaryUrlBase = 'https://shellcheck.storage.googleapis.com';
 
 export = {
     getTaskLibGetInputStub,
     getTaskLibSetResultStub,
     getTaskLibDebugStub,
     getTaskLibGetVersionInputStub,
+    getTaskLibGetVariableStub,
+    getTaskLibMkdirPStub,
+    getTaskLibToolStub,
     getOsTypeStub,
+    getOsArchStub,
+    getPathJoinStub,
+    toolRunnerStub,
+    getToolRunnerArgStub,
+    getToolRunnerArgIfStub,
+    getToolRunnerExecStub,
+    getToolLibPrependPathStub,
+    getToolLibDownloadStub,
     getInvalidVersionErrorMessage,
     failedResult,
-    taskFatalErrorMessage
+    taskFatalErrorMessage,
+    shellCheckBinaryUrlBase
 };
